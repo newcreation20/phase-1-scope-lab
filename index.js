c280a03658ad56cbc903aadc4f04535891c1f0cc
Kerny <kerny14@gmail.com>
2023-02-17 14:24:36 -0500

    commit tree 8889bedb0b96b839c1235702964ff93f058474c5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+var bestCustomer;
+
+function setBestCustomer() {
+	return bestCustomer = "not bob";
+}
+
+setBestCustomer();
+
+function overwriteBestCustomer() {
+	return bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "harry";
+
+function changeLeastFavoriteCustomer() {
+	leastFavoriteCustomer = "hank";
+}
